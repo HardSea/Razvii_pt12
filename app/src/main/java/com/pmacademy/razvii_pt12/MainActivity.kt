07975c089992bb493e76bb92dc26bca531010b53
HardSea <androidmen2000@gmail.com>
2021-01-13 00:25:18 +0200

    Initial commit

---
+++ app/src/main/java/com/pmacademy/razvii_pt12/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.pmacademy.razvii_pt12
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rv = findViewById<RecyclerView>(R.id.recyclerView)
+        rv.layoutManager = LinearLayoutManager(this)
+    }
+}
